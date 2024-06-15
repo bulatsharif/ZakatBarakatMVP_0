049890333713529d88b7caff62f2636114574e5d
bulatsharif <b.sharipov@innopolis.university>
2024-06-15 23:27:04 +0300

    - Added Calculator - Added Zakat on Property - Added Zakat on Livestock - Added Zakat Ushr - Added Nisab Value

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 from src.knowledge_base_editor.router import router as router_editor
 from src.knowledge_base_user.router import router as router_user
+from src.calculator.router import router as router_calculator
 app = FastAPI()
 
 app.include_router(router_editor)
 app.include_router(router_user)
-
-
+app.include_router(router_calculator)
 
 
 
